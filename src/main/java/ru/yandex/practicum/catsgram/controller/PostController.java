315c71302416fbf997f9707d91bf3d0045be5ff5
PracticumJava <ragozina.anastasiia@yandex.ru>
2022-04-19 16:56:56 +0300

    feature: добавить PostService

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,25 +1,24 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class PostController {
-    private static final Logger log = LoggerFactory.getLogger(PostController.class);
-
     private final PostService postService;
 
     @Autowired
     public PostController(PostService postService) {
         this.postService = postService;
     }
+
     @GetMapping("/posts")
     public List<Post> findAll() {
         return postService.findAll();
@@ -28,10 +27,5 @@ public class PostController {
     @PostMapping(value = "/post")
     public Post create(@RequestBody Post post) {
         return postService.create(post);
-    }
-
-    @GetMapping("/posts/{postId}")
-    public Post getPost(@PathVariable int postId) {
-        return postService.getPost(postId).orElse(null);
     }
 }
