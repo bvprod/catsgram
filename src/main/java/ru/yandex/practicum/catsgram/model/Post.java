7c5888b722be331afd81aeefc67fd4b812df2e7e
kpmy <petryxa.clever@gmail.com>
2022-04-19 19:54:51 +0300

    тема Параметры запроса и формат ответа, урок 1

--- src/main/java/ru/yandex/practicum/catsgram/model/Post.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/Post.java
@@ -3,17 +3,25 @@ package ru.yandex.practicum.catsgram.model;
 import java.time.Instant;
 
 public class Post {
+
     private Integer id;
     private final String author; // автор
     private final Instant creationDate = Instant.now(); // дата создания
     private String description; // описание
     private String photoUrl; // url-адрес фотографии
 
-    public Post(Integer id, String author, String description, String photoUrl) {
-        this.id = id;
+    public Post(String author, String description, String photoUrl) {
         this.author = author;
         this.description = description;
         this.photoUrl = photoUrl;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getAuthor() {
@@ -38,13 +46,5 @@ public class Post {
 
     public void setPhotoUrl(String photoUrl) {
         this.photoUrl = photoUrl;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 }
