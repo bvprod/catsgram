67807eb00bb9b98823b8d1992c05a01b16a298a0
Ярослав Большаков <yvbv1999@yandex.ru>
2024-03-21 18:10:06 +0300

    feat: added feed

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostFeedController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostFeedController.java
@@ -8,13 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.catsgram.exception.InvalidFeedRequestBody;
-import ru.yandex.practicum.catsgram.model.FriendsFeed;
 import ru.yandex.practicum.catsgram.model.Post;
-import ru.yandex.practicum.catsgram.model.User;
 import ru.yandex.practicum.catsgram.service.PostService;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -33,7 +29,8 @@ public class PostFeedController {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             String feedAsStr = objectMapper.readValue(requestBody, String.class);
-            Map<String,Object> o = objectMapper.readValue(feedAsStr, new TypeReference<>() {});
+            Map<String, Object> o = objectMapper.readValue(feedAsStr, new TypeReference<>() {
+            });
 
             if (!o.containsKey("size") || !o.containsKey("sort") || !o.containsKey("friends")) {
                 throw new InvalidFeedRequestBody("Тело запроса не содержит необходимые параметры");
